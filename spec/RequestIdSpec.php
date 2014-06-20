932a85e0b9a8aeab65911730205727bfe6fb649a
Alberto Ramirez <alberto@aramirez.es>
2014-06-20 21:30:16 +0200

    added generateFromOther

--- spec/RequestIdSpec.php
+++ spec/RequestIdSpec.php
@@ -37,9 +37,54 @@ class RequestIdSpec extends ObjectBehavior
     	$this->generate()->shouldBeAnInstanceOf('RequestId');
     }
 
+    function it_should_generate_new_request_id_object_from_other()
+    {
+    	$this->generateFromOther(new \RequestId("1m3ne2"))->__toString()->shouldEndsWith(';1m3ne2');
+    	$this->generateFromOther(
+    		new \RequestId("1m3ne2", 
+    			new \RequestId("4234ds", 
+    				new \RequestId("09942a")
+    			)
+    		)
+    	)->__toString()->shouldEndsWith(';09942a');
+    }
+
     function it_should_say_if_two_objects_are_equals()
     {
     	$this->isEqualTo(new \RequestId($this->string_id))->shouldReturn(true);
     	$this->isEqualTo(new \RequestId("some explicit id"))->shouldReturn(false);
+    }
+
+    function it_should_display_concatenated_two_requests()
+    {
+    	$request1_string	= "938f93";
+    	$request1_id		= new \RequestId($request1_string);
+
+    	$this->beConstructedWith($this->string_id, $request1_id);
+    	$this->__toString()->shouldReturn("{$this->string_id};{$request1_string}");
+    }
+
+    function it_should_display_concatenated_n_requests()
+    {
+    	$request1_string	= "938f93";
+    	$request1_id		= new \RequestId($request1_string);
+
+    	$request2_string	= "3mk8rr";
+    	$request2_id		= new \RequestId($request2_string, $request1_id);
+
+    	$request3_string	= "kij12b";
+    	$request3_id		= new \RequestId($request3_string, $request2_id);
+
+    	$this->beConstructedWith($this->string_id, $request3_id);
+    	$this->__toString()->shouldReturn("{$this->string_id};{$request3_string};{$request2_string};{$request1_string}");
+    }
+
+    public function getMatchers()
+    {
+        return [
+            'endsWith' => function($subject, $value) {
+                return preg_match("/{$value}/", $subject);
+            },
+        ];
     }
 }
--- src/RequestId.php
+++ src/RequestId.php
@@ -3,19 +3,25 @@
 class RequestId
 {
 	private $value;
+	private $parent;
 
-    public function __construct($value)
+	private static $requests_separator = ';';
+
+    public function __construct($value, RequestId $parent = null)
     {
-    	if (!is_string($value)) {
-    		$given_type = gettype($value);
-    		throw new \InvalidArgumentException("Request-ID must be a string, \"{$given_type}\" given.");
-    	}
+    	$this->guardValue($value);
     	$this->value = $value;
+    	$this->parent = $parent;
     }
 
     public static function generate()
     {
-		return new self(sha1(uniqid()));    	
+		return new self(self::generateUUID());
+    }
+
+    public function generateFromOther(RequestId $parent)
+    {
+		return new self(self::generateUUID(), $parent);
     }
 
     public function isEqualTo($other)
@@ -25,6 +31,20 @@ class RequestId
 
     public function __toString()
     {
-        return $this->value;
+    	if (empty($this->parent)) {
+    		return $this->value;
+    	}
+        return implode(self::$requests_separator, array($this->value, $this->parent));
+    }
+
+    private function guardValue($value) {
+    	if (!is_string($value)) {
+    		$given_type = gettype($value);
+    		throw new \InvalidArgumentException("Request-ID must be a string, \"{$given_type}\" given.");
+    	}
+    }
+
+    private static function generateUUID() {
+    	return sha1(uniqid());
     }
 }
