03d920121453e05286893fb1ea4027e762747b1b
Alberto Ramirez <alberto@aramirez.es>
2014-06-20 20:44:17 +0200

    added constructor, comparator, toString

--- spec/RequestIdSpec.php
+++ spec/RequestIdSpec.php
@@ -7,8 +7,39 @@ use Prophecy\Argument;
 
 class RequestIdSpec extends ObjectBehavior
 {
+	private $string_id = '8614afff87149f0e72f5dfc115ca1fdcaa1f76e3';
+
+	function let()
+	{
+		$this->beConstructedWith($this->string_id);
+	}
+
     function it_is_initializable()
     {
         $this->shouldHaveType('RequestId');
+    }
+
+    function it_displays_itself()
+    {
+    	$this->__toString()->shouldReturn($this->string_id);
+    }
+
+    function it_returns_and_error_if_value_is_not_a_string()
+    {
+    	$this->shouldThrow('Exception')->during('__construct', array(1,2));
+    	$this->shouldThrow('Exception')->during('__construct', array(-2));
+    	$this->shouldThrow('Exception')->during('__construct', array(new \stdClass));
+    	$this->shouldThrow('Exception')->during('__construct', array(true));
+    }
+
+    function it_should_generate_new_request_id_object()
+    {
+    	$this->generate()->shouldBeAnInstanceOf('RequestId');
+    }
+
+    function it_should_say_if_two_objects_are_equals()
+    {
+    	$this->isEqualTo(new \RequestId($this->string_id))->shouldReturn(true);
+    	$this->isEqualTo(new \RequestId("some explicit id"))->shouldReturn(false);
     }
 }
--- src/RequestId.php
+++ src/RequestId.php
@@ -2,4 +2,29 @@
 
 class RequestId
 {
+	private $value;
+
+    public function __construct($value)
+    {
+    	if (!is_string($value)) {
+    		$given_type = gettype($value);
+    		throw new \InvalidArgumentException("Request-ID must be a string, \"{$given_type}\" given.");
+    	}
+    	$this->value = $value;
+    }
+
+    public static function generate()
+    {
+		return new self(sha1(uniqid()));    	
+    }
+
+    public function isEqualTo($other)
+    {
+    	return $this->value === $other->value;
+    }
+
+    public function __toString()
+    {
+        return $this->value;
+    }
 }
